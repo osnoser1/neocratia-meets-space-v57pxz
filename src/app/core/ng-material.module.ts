5e88f82ca3dfb650c83fabddd9bf66a4865a5e90
Alfonso Andrés López Molina <osnoser_12@hotmail.com>
2020-08-19 15:25:43 -0500

    build: angular 9

--- src/app/core/ng-material.module.ts
+++ src/app/core/ng-material.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
-import {
-  MatButtonModule,
-  MatCommonModule,
-  MatIconModule,
-  MatListModule,
-  MatSidenavModule,
-  MatToolbarModule,
-  MatCardModule
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCommonModule } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 const matComponents = [
   MatButtonModule,
